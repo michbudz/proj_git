85b380eaf57ef1f7f0c79c61669141659ac4dc6a
Michal Budzilo <budzik88@gmail.com>
2011-04-07 16:58:42 +0200

    poprawka skrypt.php

--- skrypt.php
+++ skrypt.php
@@ -11,17 +11,17 @@ $imie=$_POST['imie'];
 $nazwisko=$_POST['nazwisko'];
 $telefon=$_POST['tel'];
 
-if($imie='michal' && $nazwisko='budzilo')
+if($imie=='michal' && $nazwisko=='budzilo')
 {
 
 ?>
-<h1 class="naglowek"> zalogowany poprawnie < /h1>
+<h1 class="naglowek"> zalogowany poprawnie </h1>
 <?php
 }
 else
 {
 ?>
-<h1 class="naglowek"> bledne logowanie < /h1>
+<h1 class="naglowek"> bledne logowanie </h1>
 <a href="index.html> powrót </a>
 <? }?>
 
